551e298ed2ab5ad60b90d6e280b75e68a5c1ec95
RyoheiMimura <51686052+RMimura@users.noreply.github.com>
2019-09-27 18:20:01 +0900

    D得点の自動計算を追加

--- IAT/IAT_with_picture.py
+++ IAT/IAT_with_picture.py
@@ -15,10 +15,10 @@ A_words=["罵倒","陰口","排斥","脅迫"]
 B_concept="非攻撃的"
 B_words=["介抱","賛美","談笑","握手"]
 
-# 概念X(png形式の写真を使用。このプログラムと同じフォルダー内に画像を保存してください。)
+# 概念X(写真を使用)
 X_concept="自己"
 X_words=["eg.png","eg2.png"]
-# 概念Y（png形式の写真を使用。このプログラムと同じフォルダー内に画像を保存してください。）
+# 概念Y（写真を使用）
 Y_concept="他者"
 Y_words=["eg3.png","eg4.png"]
 
@@ -42,7 +42,7 @@ sound=sound.Sound("A",secs=0.5)
 # データファイル
 fn_IAT=param_dict["subj_num"] +"_"+ param_dict["gender"] +"_"+ param_dict["age"] +"_"+ param_dict["counter_balance"]+"_IAT"+ ".csv"
 datafile=codecs.open(fn_IAT,"w","shift-JIS")
-#左の列から、ブロック番号、一つの刺激に対する誤答回数、反応時間、そのブロックでは左右のどちらに分類したか、刺激単語がどの概念に含まれるか、刺激単語は何かをデータファイルに出力
+# 左の列から、ブロック番号、一つの刺激に対する誤答回数、反応時間、そのブロックでは左右のどちらに分類したか、刺激単語がどの概念に含まれるか、刺激単語は何かをデータファイルに出力
 datafile.write("block,error_num,RT,L_R,word_concept,word\n")
 
 
@@ -59,39 +59,35 @@ datafile.write("block,error_num,RT,L_R,word_concept,word\n")
 #13 B-A
 #14 Y-X
 #15 BY-AX(practice) -> #16 BY-AX
-
+#
 #刺激単語
-##group A
+#group A
 #A_words
-##group B
+#group B
 #B_words
-### group A&B
+## group A&B
 AB_words=A_words+B_words
 
-##group X
-X_words
-##group Y
-Y_words
-##group X&Y
+#group X
+#X_words
+#group Y
+#Y_words
+#group X&Y
 XY_words=X_words+Y_words
 
-## all words for association
+# all words for association
 All_words_practice=AB_words+XY_words
 All_words=AB_words+XY_words
 
-#repetition & shuffle
+#repetition
 AB_words *=3
-random.shuffle(AB_words)
 XY_words *=3
-random.shuffle(XY_words)
 
 #連合ブロックの練習繰り返し数を2回とする
 All_words_practice *=2
-random.shuffle(All_words_practice)
 
 #連合ブロックの繰り返し数は3回
 All_words *=3
-random.shuffle(All_words)
 
 
 #教示文
@@ -136,7 +132,7 @@ false=visual.TextStim(win,text="×",font="ipagothic",color="red",pos=(0,0),heigh
 #--------------------------------
 
 #------------------------------------------------------------------------
-                     #実験開始
+#                     #実験開始
 #------------------------------------------------------------------------
 
 IAT.draw()
@@ -146,6 +142,12 @@ event.waitKeys()
 
 
 for block in range(1,17):
+    # shuffle
+    random.shuffle(AB_words)
+    random.shuffle(XY_words)
+    random.shuffle(All_words_practice)
+    random.shuffle(All_words)
+    
     if block in [1,3,4,9,11,12]:
         concept_A.setPos(upLeft)
         concept_B.setPos(upRight)
@@ -348,3 +350,126 @@ thankyou.draw()
 win.flip()
 event.waitKeys(keyList=["space"])
 print("Done Experiment")
+
+
+
+import pandas as pd
+import numpy as np
+import glob
+
+file = glob.glob(fn_IAT)
+
+
+# データファイルを選択
+file_mat=pd.read_csv(file[0],encoding="Shift-JIS")
+
+## 使用するデータの形成
+# 使用するブロックだけ抽出するための型
+matA=np.zeros([len(file_mat),3])
+matA[:,:]=np.nan
+matA=pd.DataFrame(matA,columns=["block", "error","rt"])
+
+# 使用するブロックのみのブロック番号、正誤、反応時間を抽出
+for row in range(0,len(file_mat)):
+    if file_mat.iloc[row,0] in ["block3","block4","block7","block8","block11","block12","block15","block16"]:
+        for col in range(0,3):
+            matA.iloc[row,col]=file_mat.iloc[row,col]
+
+# 使用しないブロックを削除
+matA=matA.dropna()
+print("Datafile was selected.")
+
+## 反応時間が300 ms未満の試行が10%以上ある参加者を排除
+n=0
+for i in range(0,len(matA)):
+    if matA.iloc[i,2] < 300:
+        n +=1
+        matA.iloc[i,2]=np.nan
+    elif matA.iloc[i,2] < 400: #400 ms未満の試行を削除
+        matA.iloc[i,2] =np.nan
+    elif matA.iloc[i,2] >= 10000: #10 s以上の試行を削除
+        matA.iloc[i,2]=np.nan
+
+under_300_sec= n/len(matA)*100
+if under_300_sec >= 10:
+    print("His/Her RT was too fast to use!!")
+    print(str(round(under_300_sec,2)) + "%")
+    D_score=np.nan
+else: # 300 ms未満の反応時間が10 %未満だった場合は以下へ進む
+    print("Calculating D-score...")
+    print(str(round(under_300_sec,2)) + "%")
+    
+    # D得点の計算
+    # 平均や標準偏差を記入する表を作成
+    matB=np.zeros([12,5])
+    matB[:,:]=np.nan
+    matB=pd.DataFrame(matB)
+    matB.iloc[:,0]=["block3","block4","block7","block8","block11","block12","block15","block16","block3_7","block4_8","block11_15","block12_16"]
+    
+    # 計算対象をリストに挿入
+    correct_b=[[],[],[],[],[],[],[],[],[],[],[],[]]
+    for rowB in range(0,8):
+        for rowA in range(0,len(matA)):
+            if matA.iloc[rowA,0]==matB.iloc[rowB,0] and matA.iloc[rowA,1]==0.0:
+                correct_b[rowB].append(matA.iloc[rowA,2])
+    for rowB2 in range(0,2):
+        correct_b[rowB2+8]=correct_b[rowB2]+correct_b[rowB2+2]
+    for rowB3 in range(0,2):
+        correct_b[rowB3+10]=correct_b[rowB3+4]+correct_b[rowB3+6]
+    
+    # 対象ブロックそれぞれの平均値と標準偏差を算出
+    for c_b_len in range(0,len(correct_b)):
+        matB.iloc[c_b_len,1]=np.nanmean(correct_b[c_b_len])
+        matB.iloc[c_b_len,2]=np.nanstd(correct_b[c_b_len])
+    
+    # 対ブロックの標準偏差を算出
+    for rowB in range(0,12):
+        matB.iloc[rowB,3]=matB.iloc[rowB,1]+2*matB.iloc[rowB,2]
+    
+    # 誤答をそのブロックの平均値＋2SDに置換
+    for rowA in range(0,len(matA)):
+        for rowB in range(0,8):
+            if matA.iloc[rowA,1] != 0.0 and matA.iloc[rowA,0]==matB.iloc[rowB,0]:
+                matA.iloc[rowA,2]=matB.iloc[rowB,3]
+    
+    #置換した後の数値も含んでブロックごとに平均化
+    b=[[],[],[],[],[],[],[],[]]
+    for rowB in range(0,8):
+        for rowA in range(0,len(matA)):
+            if matA.iloc[rowA,0]==matB.iloc[rowB,0]:
+                b[rowB].append(matA.iloc[rowA,2])
+    
+    # 置換も含めた平均を表に代入
+    for len_b in range(0,len(b)):
+        matB.iloc[len_b,4]=np.nanmean(b[len_b])
+    
+    #対ブロックの平均の差を対ブロックの標準偏差で割る
+    # (AXBY - AYBX)/SD
+    #4対の平均を D得点とする
+    if param_dict["counter_balance"]=="CB1":
+        D_score=np.mean([
+        (matB.iloc[2,4]-matB.iloc[0,4])/matB.iloc[8,2],
+        (matB.iloc[3,4]-matB.iloc[1,4])/matB.iloc[9,2],
+        (matB.iloc[4,4]-matB.iloc[6,4])/matB.iloc[10,2],
+        (matB.iloc[5,4]-matB.iloc[7,4])/matB.iloc[11,2]])
+    else:
+        D_score=np.mean([
+        (matB.iloc[0,4]-matB.iloc[2,4])/matB.iloc[8,2],
+        (matB.iloc[1,4]-matB.iloc[3,4])/matB.iloc[9,2],
+        (matB.iloc[6,4]-matB.iloc[4,4])/matB.iloc[10,2],
+        (matB.iloc[7,4]-matB.iloc[5,4])/matB.iloc[11,2]])
+
+matC=np.zeros([1,6])
+matC[:,:]=np.nan
+matC=pd.DataFrame(matC,columns=["subj_num","sex","age","CB","under 300 ms(%)","D_score"])
+
+matC.iloc[0,0]=param_dict["subj_num"]
+matC.iloc[0,1]=param_dict["gender"]
+matC.iloc[0,2]=param_dict["age"]
+matC.iloc[0,3]=param_dict["counter_balance"]
+matC.iloc[0,4]=under_300_sec
+matC.iloc[0,5]=D_score
+fn_IAT_D=param_dict["subj_num"] +"_"+ param_dict["gender"] +"_"+ param_dict["age"] +"_"+ param_dict["counter_balance"]+"_IAT_Dscore"+ ".csv"
+matC.to_csv(fn_IAT_D)
+print("---D_score (Greenwald, Nosek, & Banaji, 2003)---")
+print(round(D_score,3))
